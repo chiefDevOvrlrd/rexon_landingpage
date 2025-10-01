6795609bfe00661e50a7297f2beade0c7d6ff9b8
joeatitagain <anetojoseph@gmail.com>
2025-10-01 11:08:08 +0100

    Fixed typo in Pricing page: 'Let's get rolling!!!' -> 'Let's get rolling!!

--- rexon_dev/app/pricing/page.tsx
+++ rexon_dev/app/pricing/page.tsx
@@ -213,7 +213,7 @@ const Pricing = () => {
             </div>
             <div className={styles.pricing__getStarted}>
                 <h2>Let&apos;s get rolling!!!</h2>
-                <p>The best way to get an accurate quote is to discuss your project with us. Let's schedule a free consultation to talk about your vision and provide a personalized investment estimate.</p>
+                <p>The best way to get an accurate quote is to discuss your project with us. Let&apos;s schedule a free consultation to talk about your vision and provide a personalized investment estimate.</p>
                 <div className={styles.pricing__getStarted__button}>
                     <BlackButton text="Start your Dream"/>
                 </div>
